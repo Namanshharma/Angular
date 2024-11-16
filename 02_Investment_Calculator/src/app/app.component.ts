7df0c7924c48696339eef7db6e5af20466cfe11e
namanshharma <namansharma812@gmail.com>
2024-11-17 05:07:04 +0530

    Without service everything is working

--- 02_Investment_Calculator/src/app/app.component.ts
+++ 02_Investment_Calculator/src/app/app.component.ts
@@ -2,20 +2,22 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { UserInputComponent } from "./user-input/user-input.component";
-import { calculatedDataIn, calculatedDataOut } from '../Models';
+import type { calculatedDataIn, calculatedDataOut } from '../Models';
+import { InvestmentResultsComponent } from "./investment-results/investment-results.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, UserInputComponent],
+  imports: [RouterOutlet, HeaderComponent, UserInputComponent, InvestmentResultsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Investment_Calculator';
+  investmentGridData !: calculatedDataOut[];
 
   onCalculateInvestment(data: calculatedDataIn) {
-    this.calculateInvestmentResults(data);
+    this.investmentGridData = this.calculateInvestmentResults(data);
   }
 
   calculateInvestmentResults(data: calculatedDataIn): calculatedDataOut[] {
--- 02_Investment_Calculator/src/app/user-input/user-input.component.ts
+++ 02_Investment_Calculator/src/app/user-input/user-input.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { calculatedDataIn } from '../../Models';
 
 @Component({
   selector: 'app-user-input',
@@ -9,13 +10,22 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './user-input.component.css'
 })
 export class UserInputComponent {
-  @Output() calculateInvestment: EventEmitter<any> = new EventEmitter<any>();
+  @Output() calculateInvestment: EventEmitter<calculatedDataIn> = new EventEmitter<calculatedDataIn>();
   InitialInvestment: string | number = "0";
   AnnualInvestment: string | number = "0";
   Duration: string | number = "0";
   ExpectedReturn: string | number = "0";
 
   onSubmit(): void {
-
+    this.calculateInvestment.emit({
+      initialInvestment: Number(this.InitialInvestment),
+      annualInvestment: Number(this.AnnualInvestment),
+      duration: Number(this.Duration),
+      expectedReturn: Number(this.ExpectedReturn)
+    })
+    this.InitialInvestment = 0;
+    this.AnnualInvestment = 0;
+    this.Duration = 0;
+    this.ExpectedReturn = 0;
   }
 }
