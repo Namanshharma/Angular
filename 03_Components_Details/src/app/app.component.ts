f40519fdf8f236c118a1609a9f6c52655409b0d6
namanshharma <namansharma812@gmail.com>
2024-11-17 05:40:17 +0530

    Initial push

--- 03_Components_Details/src/app/app.component.ts
+++ 03_Components_Details/src/app/app.component.ts
@@ -9,5 +9,37 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = '03_Components_Details';
+  title = 'Components_Details';
+  dummyTrafficData = [
+    {
+      id: 'd1',
+      value: 433,
+    },
+    {
+      id: 'd2',
+      value: 260,
+    },
+    {
+      id: 'd3',
+      value: 290,
+    },
+    {
+      id: 'd4',
+      value: 410,
+    },
+    {
+      id: 'd5',
+      value: 397,
+    },
+    {
+      id: 'd6',
+      value: 488,
+    },
+    {
+      id: 'd47',
+      value: 589,
+    },
+  ];
+  maxTraffic = Math.max(...this.dummyTrafficData.map((data) => data.value));
+  currentStatus = 'online';
 }
