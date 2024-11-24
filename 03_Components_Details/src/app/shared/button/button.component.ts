bceceb953fb6ffd83a7150572647bb3c26b9597c
Naman Sharma <namansharma812@gmail.com>
2024-11-25 02:37:19 +0530

    Code implementation

--- 03_Components_Details/src/app/shared/button/button.component.ts
+++ 03_Components_Details/src/app/shared/button/button.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
 
+// there are different types of selector are present ( attribute selector ( [appButton] ) , element selector (app-button or button) there are the example of element selector)
+// also we can combine both of these selectors as well.
 @Component({
-  selector: 'app-button',
+  selector: 'button[appButton] , a[appButton]',
   standalone: true,
   imports: [],
   templateUrl: './button.component.html',
