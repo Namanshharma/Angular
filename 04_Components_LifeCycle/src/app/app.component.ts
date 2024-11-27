521350f0504cbfa1c8aa9653f6f98cc344ccc8e7
Naman Sharma <namansharma812@gmail.com>
2024-11-28 02:58:51 +0530

    Final commit of Life cycle project

--- 04_Components_LifeCycle/src/app/app.component.ts
+++ 04_Components_LifeCycle/src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { LifeCycleComponent } from "./Components/life-cycle/life-cycle.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LifeCycleComponent],
+  imports: [LifeCycleComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
