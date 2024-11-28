65471b26e9c9c2679174173c53d8b8fceeb9aa2c
Naman Sharma <namansharma812@gmail.com>
2024-11-29 04:08:43 +0530

    Code implementation for template variables along with ViewChild decorator

--- 03_Components_Details/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ 03_Components_Details/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { ButtonComponent } from "../../../shared/button/button.component";
 import { ControlComponent } from "../../../shared/control/control.component";
 
@@ -10,7 +10,20 @@ import { ControlComponent } from "../../../shared/control/control.component";
   styleUrl: './new-ticket.component.css'
 })
 export class NewTicketComponent {
-  onSubmit(): void {
+  @ViewChild('form') form?: ElementRef<HTMLFormElement>;    // by using this view Child decorator we can access the every variable in the template also we can use the component as well.
 
+  enteredTitle !: string;
+  enteredText !: string;
+
+  // onSubmit(title: HTMLInputElement, text: HTMLTextAreaElement, form: HTMLFormElement): void {
+  //   this.enteredTitle = title;
+  //   this.enteredText = text;
+  //   form.reset();
+  // }
+
+  onSubmit(title: string, text: string): void {
+    this.enteredTitle = title;
+    this.enteredText = text;
+    this.form?.nativeElement.reset();
   }
 }
