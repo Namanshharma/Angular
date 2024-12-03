125b06a7bc8e49b67941d41eaecb3909a00a6efd
Naman Sharma <namansharma812@gmail.com>
2024-12-04 02:11:02 +0530

    First commit

---
+++ 05_Custom_TwoWay_Binding/src/app/rect/rect.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-rect',
+  standalone: true,
+  imports: [],
+  templateUrl: './rect.component.html',
+  styleUrl: './rect.component.css'
+})
+export class RectComponent {
+  onReset() {
+    // ...
+  }
+}
